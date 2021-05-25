1a27c99ac1f7daf67752a222647c6b6fe16af684
Gustavo Costa <gucostarp@gmail.com>
2021-05-25 13:32:03 -0300

    Projeto de Boas Práticas de Programação

---
+++ src/views/pages/components/footer/index.js
@@ -0,0 +1,14 @@
+import Planet from '../../../../assets/images/elements/planet.png'
+
+let footer = `
+<div class="footer-contain">
+    <p>
+        Star wars  -  Este layout foi criado durante o curso ministrado pela GAMA Academy para a Afya Labs. Gustavo Costa.
+    </p>
+    <a href="#section-one">
+        <img src="${Planet}" alt="planet">
+    </a>
+</div>
+`
+
+export default footer;
---
+++ src/views/pages/index.js
@@ -0,0 +1,13 @@
+//errors
+import Error404 from './Errors/Error404';
+
+//Home
+import Home from './home/Home';
+
+//Jedi
+import Jedi from './jedi';
+
+// exportar
+export const home = Home;
+export const error404 = Error404;
+export const jedi = Jedi;
